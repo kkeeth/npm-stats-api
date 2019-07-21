4cdc317d52059f8fd7c225fc2d89eaeab40b27d1
k-kuwahara <zensin0082@gmail.com>
2019-07-21 22:11:45 +0900

    feat: add 'details' method's sample

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const npm = require('./index')
 npm.stat('check-stats-modules', '2018-01-01', '2019-05-01', (err, res) => {
    console.log(JSON.stringify(res))
 })
+
+npm.details('check-stats-modules', (err, res) => {
+  console.dir(res.description)
+})
