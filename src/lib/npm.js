6405ad9c41b7fa1963061a3c227f09d83670ac92
kkeeth <zensin0082@gmail.com>
2020-08-07 00:33:00 +0900

    refactor: formatted by prettier

--- src/lib/npm.js
+++ src/lib/npm.js
@@ -1,36 +1,39 @@
-const request = require('request')
+const request = require("request");
 
 module.exports = {
-   load: function(url, cb) {
-      request({
-         url: url,
-         json: true
-      }, (err, res, body) => {
+  load: function(url, cb) {
+    try {
+      if (cb === undefined) throw "callback function is a required argument";
+
+      request({ url: url, json: true }, (err, res, body) => {
         if (!err) {
-           let message = null
-           switch(res.statusCode) {
-              case 400:
-                 message = 'Invalid data'
-                 break
-              case 404:
-                 message = 'Package not found'
-                 break
-              case 412:
-                 message = 'Precondition failed'
-                 break
-           }
-           cb(message, body)
+          let status = null;
+          switch (res.statusCode) {
+            case 400:
+              status = "Invalid data";
+              break;
+            case 404:
+              status = "Package not found";
+              break;
+            case 412:
+              status = "Precondition failed";
+              break;
+          }
+          cb(status, body);
         } else {
-          cb('Error', 'An error has occurred. Please try again later, sorry')
+          throw err.message;
         }
-      })
-   },
-   stat: function(pkg, start, end, cb) {
-      const url = `https://api.npmjs.org/downloads/point/${start}:${end}/${pkg}`
-      this.load(url, cb)
-   },
-   details: function(pkg, cb) {
-      const url = `https://registry.npmjs.org/${pkg}`
-      this.load(url, cb)
-   }
-}
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
+  },
+  stat: function(pkg, start, end, cb) {
+    const url = `https://api.npmjs.org/downloads/point/${start}:${end}/${pkg}`;
+    this.load(url, cb);
+  },
+  details: function(pkg, cb) {
+    const url = `https://registry.npmjs.org/${pkg}`;
+    this.load(url, cb);
+  },
+};
