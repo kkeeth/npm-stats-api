7a8e6cbbcd5ec2520712ffcc7ac7ce541749d329
kkeeth <zensin0082@gmail.com>
2022-02-11 10:52:25 +0900

    feat: changed logic of error handling

--- src/lib/npm.js
+++ src/lib/npm.js
@@ -2,7 +2,7 @@ const request = require("superagent");
 
 module.exports = {
   load: function(url, cb) {
-    if (cb === undefined) throw "callback function is a required argument";
+    if (cb === undefined) throw new Error("callback function is a required argument");
 
     request
       .get(url)
@@ -12,20 +12,17 @@ module.exports = {
         deadline: 5 * 1000,
       })
       .end((err, res) => {
-        // for 5xx error
         if (!res) {
-          throw new Error(err.message);
-        }
-
-        // for 4xx error
-        if (err) {
-          console.log(err.message)
+          // for 5xx error
+          cb(err, res);
+        } else if (err) {
+          // for 4xx error
           cb(err, res.body)
         } else {
           // for 200 ok
           cb(null, res.body);
         }
-      })
+      });
   },
   stat: function(pkg, start, end, cb) {
     const url = `https://api.npmjs.org/downloads/point/${start}:${end}/${pkg}`;
