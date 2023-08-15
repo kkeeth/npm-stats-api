504577607035a7b2ff18d3cc17ab44042682a813
kkeeth <zensin0082@gmail.com>
2023-08-15 12:39:36 +0900

    feat: add type define file

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { StatType, DetailType } from "./src/types";
+export declare function stat(pkg: string, start: Date, end: Date): StatType;
+export declare function details(pkg: string): DetailType;
