dfde49613814cbe3fbc3bec65f44f5b508cc525d
João Teixeira <jpedroteix99@gmail.com>
2024-04-23 13:56:04 +0100

    fix type of date parameters

--- src/lib/npm.ts
+++ src/lib/npm.ts
@@ -9,7 +9,7 @@ import type { StatType, DetailType } from "../../index.d";
  * @param {String} end: End date of search period
  * @returns Promise object
  */
-const stat = (pkg: string, start: Date, end: Date): StatType => {
+const stat = (pkg: string, start: string, end: string): StatType => {
   const url = `https://api.npmjs.org/downloads/point/${start}:${end}/${pkg ? pkg : ""}`;
   return load(url)
 };
