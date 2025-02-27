556ceb7c7c3af85a1d3462eca0a8d13792d44f92
kkeeth <zensin0082@gmail.com>
2025-02-27 20:06:30 +0900

    fix: the sample code was not working

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const npm = require("./");
+const { npm } = require("./");
 
 // 200 ok pattern about 'stat' method
 npm
   .stat("npm-stats-api", "2022-01-01", "2022-02-15")
   .then(res => {
-    console.log("\n# ok pattern about stat method");
-    console.log(res);
+    console.log("\n# ok pattern about stat method\n", res);
   })
   .catch(err => {
     console.log(err);
@@ -15,8 +14,7 @@ npm
 npm
   .details("npm-stats-api")
   .then(res => {
-    console.log("\n# ok pattern about stat method");
-    console.log(res);
+    console.log("\n# ok pattern about details method\n", res);
   })
   .catch(err => {
     console.log(err);
@@ -26,20 +24,20 @@ npm
 npm
   .stat("npm-stats-api", "hoge", "2022-02-15")
   .then(res => {
-    console.log("\n# date format error pattern about stat method");
     console.log(res);
   })
   .catch(err => {
+    console.log("\n# date format error pattern about stat method");
     console.log(err);
   });
 
-// not found error pattern about 'details' method
+// not found error pattern about 'stat' method
 npm
-  .stat("abcdefghijk", "2019-01-01", "2022-02-15")
+  .stat("abcdefghijklm", "2019-01-01", "2022-02-15")
   .then(err => {
-    console.log("\n# not found error pattern about details method");
     console.log(err);
   })
   .catch(err => {
+    console.log("\n# not found error pattern about details method");
     console.log(err);
   });
