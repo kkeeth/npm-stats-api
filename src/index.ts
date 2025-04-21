9dda0ef5ae7f994557fdfb5a89750260a8368065
Keeth Kuwahara <zensin0082@gmail.com>
2025-04-21 20:49:00 +0900

    refactor: Improve module exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export * as npm from "./lib/npm";
+import * as npmFunctions from './lib/npm';
+import { NpmException } from './lib/npmException';
+
+// Export functions
+export const stat = npmFunctions.stat;
+export const details = npmFunctions.details;
+
+// Export types and exception
+export { NpmException };
+export * from '../index.d';
